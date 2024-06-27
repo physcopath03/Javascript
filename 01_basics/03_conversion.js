e2552450a86e4bc085fdbf08f8507b00d23ad233
Anjani Kumar <166216242+physcopath03@users.noreply.github.com>
2024-06-27 10:26:44 +0000

    operations

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,26 +1,58 @@
-// let Score = "Anjani"
+// // let Score = "Anjani"
 
-// console.log(typeof Score)
+// // console.log(typeof Score)
 
-// let valueInNumber= Number(Score)
-// console.log(valueInNumber)
+// // let valueInNumber= Number(Score)
+// // console.log(valueInNumber)
 
-// // "33"=>33
-// //"33abs"=> NAN Not a number
-// // True=> 1; false=>0
+// // // "33"=>33
+// // //"33abs"=> NAN Not a number
+// // // True=> 1; false=>0
 
-// let isLoggedIN= "Anjani"
+// // let isLoggedIN= "Anjani"
 
-// let BooleanIsLoggedIn= Boolean(isLoggedIN)
-// console.log(BooleanIsLoggedIn)
+// // let BooleanIsLoggedIn= Boolean(isLoggedIN)
+// // console.log(BooleanIsLoggedIn)
 
-// 1=> True; 0=> false
-// "=> False"
-//"Anjani"=> false
+// // 1=> True; 0=> false
+// // "=> False"
+// //"Anjani"=> false
 
-let someNumber = 43
+// let someNumber = 43
 
-let stringNumber= String(someNumber)
+// let stringNumber= String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*************OPERATIONS ***********************************
+
+let value = 3
+let negvalue= -value
+//console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Anjani"
+str3= str1+str2
+// console.log(str3)
+// console.log("1" + 2 );
+// console.log("1" +2+2);
+// console.log(1 +2+"2");
+
+// console.log (+true);
+// console.log(+ "");
+
+// let num1, num2, num3
+
+// num1=num2=num3= 2+2
+
+let gameCounter =100
+
+++gameCounter;
+console.log(gameCounter);
