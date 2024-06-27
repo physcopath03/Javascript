f21ce4bd91ca09178347aab5a5105e3286ff7339
Anjani Kumar <166216242+physcopath03@users.noreply.github.com>
2024-06-27 10:07:32 +0000

    conversions

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,19 @@
+// let Score = "Anjani"
+
+// console.log(typeof Score)
+
+// let valueInNumber= Number(Score)
+// console.log(valueInNumber)
+
+// "33"=>33
+//"33abs"=> NAN Not a number
+// True=> 1; false=>0
+
+let isLoggedIN= "Anjani"
+
+let BooleanIsLoggedIn= Boolean(isLoggedIN)
+console.log(BooleanIsLoggedIn)
+
+// 1=> True; 0=> false
+// "=> False"
+//"Anjani"=> false
