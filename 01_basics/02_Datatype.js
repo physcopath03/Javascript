ff88a40b1fc5286f676583b79dc865c6a56eb133
Anjani Kumar <166216242+physcopath03@users.noreply.github.com>
2024-10-22 08:20:03 +0000

    alert use

--- 01_basics/02_Datatype.js
+++ 01_basics/02_Datatype.js
@@ -1,23 +1,3 @@
-"Use Strict"; // treate all Js code as newer version of Js
-// alert(3 + 3) // we are using node Js here not the browser
+"use Strict"; // treat all js code as newer version
 
-console.log(3 + 3)  // code readability should be high 
-console.log("Anjani")
-
-let name= "Anjani" // string
-let age = 22 // integer
-let isLoggedIN= false // boleean
-// premitive data type
-// 1. Number => 2^53
-// bigint if the number is too long
-// string can be use in single or doulble quotes  string=>""
-// Boolean=> true/ false
-null it is basically a standalone value
-undefined=> when the valus is nit define as used in variable let state=""
-basically diffence between null and undefined is null means it store some value but undefined means it is not define means empty
-symbol=> unique
-
-// Object
-
-console.log(typeof undefined) // undefined
-console.log(typeof null)// object
+alert(3 +3) // we are using node js not browser that's why it is not running
