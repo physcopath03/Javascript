cfa3ce1be3d7ac9a6097a34d5e5773edecd9b163
Anjani Kumar <166216242+physcopath03@users.noreply.github.com>
2024-10-22 08:34:12 +0000

    datatypes

--- 01_basics/02_Datatype.js
+++ 01_basics/02_Datatype.js
@@ -1,3 +1,21 @@
 "use Strict"; // treat all js code as newer version
+//alert(3 +3) // we are using node js not browser that's why it is not running
 
-alert(3 +3) // we are using node js not browser that's why it is not running
+console.log(3+3)
+let name="Anjani" // it is now string datatype
+let age = 18 // int datatype
+let isloggedIN = false // boolean
+// number =>2 to power 53
+//bigint
+//string=>""
+//boolean => true/false
+// null=> it is representation of empty value or it is standalone value
+//undefined=> no value is assigned
+//symbol => unique
+
+//Object
+
+// we can find out the which type of datatype we are using with the help of "typeof" use;
+
+console.log( typeof null); // output will be object
+console.log( typeof undefined); // output will be undefined
