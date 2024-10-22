55ef5cfa58cd7ad8e011399590994426c6e81679
Anjani Kumar <166216242+physcopath03@users.noreply.github.com>
2024-10-22 09:00:33 +0000

    conversions of datatypes

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,58 +1,29 @@
-// let Score = "Anjani"
+let score = "33abx"
+// in undefined it is NaN and in Null it print 0 and for the other depends upon the input and if string then it will be NaN
+console.log(typeof (score))
 
-// console.log(typeof Score)
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber); //output will be NaN 
 
-// let valueInNumber= Number(Score)
-// console.log(valueInNumber)
+/*
+"33"=>33
+"33abc"=>NaN
+true=> 1; false=> 0
+*/
+let isloggedIN = 1
 
-// // "33"=>33
-// //"33abs"=> NAN Not a number
-// // True=> 1; false=>0
+let booleanIsLoggedIn = Boolean(isloggedIN);
+console.log(booleanIsLoggedIn);
 
-let isLoggedIN= "Anjani"
+/*
+1=> True; 0=> False
+""=> False
+"Anjani" => True
+*/
+let someNumber = 33
 
-let BooleanIsLoggedIn= Boolean(isLoggedIN)
-console.log(BooleanIsLoggedIn)
-
-// 1=> True; 0=> false
-// "=> False"
-//"Anjani"=> false
-
-let someNumber = 43
-
-let stringNumber= String(someNumber)
-
+let stringNumber = String(someNumber)
 console.log(stringNumber);
-console.log(typeof stringNumber);
 
-//*************OPERATIONS ***********************************
-
-let value = 3
-let negvalue= -value
-console.log(negvalue)
-
-console.log(2+2);
-console.log(2-2);
-console.log(2*2);
-console.log(2/2);
-console.log(2%2);
-
-let str1 = "Hello"
-let str2 = " Anjani"
-str3= str1+str2
-console.log(str3)
-console.log("1" + 2 );
-console.log("1" +2+2);
-console.log(1 +2+"2");
-
-console.log (+true);
-console.log(+ "");
-
-let num1, num2, num3
-
-num1=num2=num3= 2+2
-
-let gameCounter =100
-
-++gameCounter;
-console.log(gameCounter);
+console.log(typeof(stringNumber));
