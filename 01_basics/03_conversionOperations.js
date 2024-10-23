c66a1093fa9ea7975aa6d3698637e3d1d40da2e3
Anjani Kumar <166216242+physcopath03@users.noreply.github.com>
2024-10-23 06:20:27 +0000

    operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,46 @@
+// let score = "33abx"
+// // in undefined it is NaN and in Null it print 0 and for the other depends upon the input and if string then it will be NaN
+// console.log(typeof (score))
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber); //output will be NaN 
+
+// /*
+// "33"=>33
+// "33abc"=>NaN
+// true=> 1; false=> 0
+// */
+// let isloggedIN = 1
+
+// let booleanIsLoggedIn = Boolean(isloggedIN);
+// console.log(booleanIsLoggedIn);
+
+// /*
+// 1=> True; 0=> False
+// ""=> False
+// "Anjani" => True
+// */
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+
+// console.log(typeof(stringNumber));
+
+//******************************************OPERATIONS*******************************************
+
+let value= 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+
+let str1= "Hello"
+let str2 = "Anjani"
+
+let str3 = str1 + str2
+console.log(str3);
