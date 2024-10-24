1406453884ac1314d612cb3061b57dfefdbd18fd
Anjani Kumar <166216242+physcopath03@users.noreply.github.com>
2024-10-24 07:48:34 +0000

    string

---
+++ 01_basics/05_String.js
@@ -0,0 +1,29 @@
+const Name="Anjani"
+const Repocount= 50
+
+console.log( Name + Repocount +"Value");
+
+
+// string Interpulation means we can inject the varibale using ${}
+
+console.log(`Hello My Name is ${Name} and my Repcount is ${Repocount}`);
+
+const gameName= new String('Anjani')
+
+console.log(gameName[0]);
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(4));
+
+console.log(gameName.indexOf('A'));
+
+const newString = gameName.substring(0,4)
+
+console.log(newString);
+
+const mewString = gameName.slice(-6,4)
+
+console.log(mewString);
