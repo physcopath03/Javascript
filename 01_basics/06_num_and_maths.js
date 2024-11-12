db3b03698b63c5b954a12be3a9d58db549dc2b33
Anjani Kumar <166216242+physcopath03@users.noreply.github.com>
2024-11-12 07:29:03 +0000

    math and num

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,28 @@
+const score = 400
+console.log(score)
+const balance= new Number(100)
+console.log(balance);
+
+
+// tostring is use to convert the value in the string 
+console.log(balance.toString().length);
+
+// tofixed is use to provide the most precise value of the string or after the decimal value like 100.00
+console.log(balance.toFixed(2));
+
+const othernumber= 122.909934089048
+// use to provide the most precise or round off value
+console.log(othernumber.toPrecision(3));
+
+
+const hundreds =1000000
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++  Maths   +++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+
+console.log(Math.round(2.4));
